074b805e4ddb6397175c0348ece7439411d3414c
Calvin <o8454k@gmail.com>
2021-11-09 10:54:42 +0800

    first commit

---
+++ WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="SHA256.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Input:
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<br />
+            Key:&nbsp;&nbsp;
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="加密" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+        </div>
+
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.Text.RegularExpressions;
+
+namespace SHA256
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Text = CreateHMACSHA256(TextBox1.Text,TextBox2.Text);
+        }
+
+        private string CreateHMACSHA256(String inputText,String inKey)
+        {
+            var encoding = new System.Text.UTF8Encoding();
+
+            byte[] keyByte = encoding.GetBytes(inKey);
+            byte[] messageBytes = encoding.GetBytes(inputText);
+            using (var hmacSHA256 = new HMACSHA256(keyByte))
+            {
+                byte[] hashMessage = hmacSHA256.ComputeHash(messageBytes);
+                return BitConverter.ToString(hashMessage).Replace("-", "").ToLower();
+            }
+        }
+
+        private string CreateSHA256(String inputText, String inKey)
+        {
+            var encoding = new System.Text.UTF8Encoding();
+            //byte[] keyByte = encoding.GetBytes(inKey);
+            byte[] messageBytes = encoding.GetBytes(inputText);
+
+
+            System.Security.Cryptography.SHA256 testSHA256 = System.Security.Cryptography.SHA256.Create();
+
+            byte[] hashMessage = testSHA256.ComputeHash(messageBytes);
+            return BitConverter.ToString(hashMessage).Replace("-","").ToLower();
+
+        }
+
+    }
+
+}
